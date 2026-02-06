539088efa9175b7608bd60fe32de9f4f3af18555
Achim Thesmann <achim@google.com>
2026-02-05 22:37:58 -0800

    Implement UprobeStats self metrics logging
    This CL logs UprobeStats self metrics, including service invocation, internal errors and BPF program interactions.
    
    Bug: 412657349
    Test: manual
    Flag: EXEMPT METRICS
    Change-Id: Iaa3bc62128cc3eee170b37d40a96fc653555a5f4

--- daemon/src/uprobestats_service.rs
+++ daemon/src/uprobestats_service.rs
@@ -3,6 +3,7 @@
 use crate::{task, task::GlobalState};
 use binder::{Interface, Status};
 use log::{error, trace};
+use statslog_uprobestats::{uprobe_stats_internal_error, uprobe_stats_invocation};
 use std::{
     sync::{Arc, Mutex},
     thread,
@@ -30,6 +31,11 @@ impl Interface for UprobeStatsService {}
 impl IUprobeStatsService for UprobeStatsService {
     fn startTasks(&self, config: &[u8]) -> Result<(), Status> {
         trace!("received startTasks call");
+        if let Err(e) = uprobe_stats_invocation::stats_write(
+            uprobe_stats_invocation::InvocationType::InvocationTypeStartTasksReceived,
+        ) {
+            error!("Failed to write uprobe_stats_invocation atom for start tasks received: {e:?}");
+        };
 
         let config = config.to_vec();
         let state = self.state.clone();
@@ -37,6 +43,12 @@ impl IUprobeStatsService for UprobeStatsService {
             let task = match task::resolve_config(&config) {
                 Ok(task) => task,
                 Err(e) => {
+                    if let Err(e) = uprobe_stats_internal_error::stats_write(
+                        uprobe_stats_internal_error::ErrorType::ErrorTypeConfigParseFailed,
+                        0,
+                    ) {
+                        error!("Failed to write uprobe_stats_internal_error atom for config parse failure: {e:?}");
+                    };
                     error!("{e}");
                     return;
                 }
@@ -46,6 +58,12 @@ impl IUprobeStatsService for UprobeStatsService {
             {
                 let mut state = state.lock().unwrap();
                 if let Err(e) = task::update_polled_bpf_maps(&mut state, &task) {
+                    if let Err(e) = uprobe_stats_internal_error::stats_write(
+                        uprobe_stats_internal_error::ErrorType::ErrorTypeTaskConflict,
+                        task.task.task_id.unwrap_or(0),
+                    ) {
+                        error!("Failed to write uprobe_stats_internal_error atom for task conflict: {e:?}");
+                    };
                     error!("{e}");
                     return;
                 }
